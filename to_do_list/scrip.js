a2a42dbc9337eccdef6b317573d8ef5efc153963
Guilherme Costa <guilhermecostang@gmail.com>
2023-10-25 19:39:18 -0300

    conluir e excluir tarefa

--- to_do_list/scrip.js
+++ to_do_list/scrip.js
@@ -5,7 +5,11 @@ const listaCompleta = document.querySelector('.list-task');
 let minhaListaDeItens = [];
 
 function adicionarNovaTarefa() {
-    minhaListaDeItens.push(input.value);
+    minhaListaDeItens.push({
+        tarefas: input.value,
+        concluida: false
+    });
+    input.value = '';
 
     mostraTarefas();
 }
@@ -14,18 +18,28 @@ function mostraTarefas() {
 
     let novaLi = '';
 
-    minhaListaDeItens.forEach((tarefas) => {
+    minhaListaDeItens.forEach((item, posicao) => {
         novaLi = novaLi + `
-            <li class="task">
-                <img src="img/checked.png" alt="check-na-tarefa">
-                <p>${tarefas}</p>
-                <img src="img/trash.png" alt="tarefa-para-o-lixo">
+            <li class="task ${item.concluida && "done"}">
+                <img src="img/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${posicao})">
+                <p>${item.tarefas}</p>
+                <img src="img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${posicao})">
             </li>
         `
     });
 
     listaCompleta.innerHTML = novaLi;
 
+}
+
+function deletarItem(posicao) {
+    minhaListaDeItens.splice(posicao, 1);
+    mostraTarefas();
+}
+
+function concluirTarefa(posicao) {
+    minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida;
+    mostraTarefas();
 }
 
 button.addEventListener('click', adicionarNovaTarefa);
